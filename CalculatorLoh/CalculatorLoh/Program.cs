f7b8dd65c2eed76f194a2f0ec5da001b5f7228e9
Emavideno <166414249+Emavideno@users.noreply.github.com>
2025-03-15 20:55:12 +0300

    Слабый калькулятор
    Прям воняет слабостью

---
+++ CalculatorLoh/CalculatorLoh/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите через пробел(число_знак_число): ");
+string str = Console.ReadLine();
+string[] numsOper = str.Split();
+int num1 = int.Parse(numsOper[0]);
+int num2 = int.Parse(numsOper[2]);
+string oper = numsOper[1];
+if (oper == "+")
+    Console.WriteLine(num1 + num2);
+else if (oper == "-")
+    Console.WriteLine(num1 - num2);
+else if (oper == "*")
+    Console.WriteLine(num1 * num2);
+else
+    Console.WriteLine(num1 / num2);
