d5952dcd280dfa7a57d7684adc4e616d921e7685
Emavideno <166414249+Emavideno@users.noreply.github.com>
2025-03-15 21:01:47 +0300

    Крутой калькулятор
    На стиле

---
+++ CalculatorPro/CalculatorPro/Strategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorPro
+{
+    // родоначальник стратегий
+    internal interface ICount
+    {
+        double DoCount(double num1, double num2);
+    }
+}
